6794694b5705d8a17e8ab5477d5f20117c9cf583
Angelo Carlotto <angelocarlotto@gmail.com>
2024-10-15 01:41:16 -0400

    added more mock itens

--- ContactList/Data/ContactListDbContext.cs
+++ ContactList/Data/ContactListDbContext.cs
@@ -36,7 +36,14 @@ public partial class ContactListDbContext : DbContext
         // Seed data for Contact
         modelBuilder.Entity<Contact>().HasData(
             new Contact { ContactId = 1, FirstName = "John", LastName = "Doe", Phone = "+12342345544", Email = "john@example.com", CategoryId = 1,DateAdded=DateTime.Now },
-            new Contact { ContactId = 2, FirstName = "Jane", LastName = "Smith", Phone = "+99999999999", Email = "jane@example.com", CategoryId = 2, DateAdded = DateTime.Now}
+            new Contact { ContactId = 2, FirstName = "Jane", LastName = "Smith", Phone = "+99999999999", Email = "jane@example.com", CategoryId = 2, DateAdded = DateTime.Now},
+            new Contact { ContactId = 3, FirstName = "Jane3", LastName = "Smith", Phone = "+99999999999", Email = "jane3@example.com", CategoryId = 2, DateAdded = DateTime.Now},
+            new Contact { ContactId = 4, FirstName = "Jane4", LastName = "Smith", Phone = "+99999999999", Email = "jane4@example.com", CategoryId = 2, DateAdded = DateTime.Now},
+            new Contact { ContactId = 5, FirstName = "Jane5", LastName = "Smith", Phone = "+99999999999", Email = "jane5@example.com", CategoryId = 2, DateAdded = DateTime.Now},
+            new Contact { ContactId = 6, FirstName = "Jane6", LastName = "Smith", Phone = "+99999999999", Email = "jane6@example.com", CategoryId = 2, DateAdded = DateTime.Now},
+            new Contact { ContactId = 7, FirstName = "Jane7", LastName = "Smith", Phone = "+99999999999", Email = "jane7@example.com", CategoryId = 2, DateAdded = DateTime.Now},
+            new Contact { ContactId = 8, FirstName = "Jane8", LastName = "Smith", Phone = "+99999999999", Email = "jane8@example.com", CategoryId = 2, DateAdded = DateTime.Now},
+            new Contact { ContactId = 9, FirstName = "Jane9", LastName = "Smith", Phone = "+99999999999", Email = "jane9@example.com", CategoryId = 2, DateAdded = DateTime.Now}
         );
     }
 
